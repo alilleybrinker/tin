a3cf735b2041b8117b4a4c9d2823abf2bb4fc844
Andrew Lilley Brinker <andrewbrinker@me.com>
2020-10-28 18:51:11 -0700

    Moved HIR code to 'libraries/tinhir' (#3)

--- tools/tinc/src/main.rs
+++ tools/tinc/src/main.rs
@@ -1,12 +1,7 @@
-mod error;
-mod hir;
-mod parse;
-
-use crate::error::Error;
-use crate::parse::parse;
 use anyhow::Result;
 use std::env::args_os;
 use std::fs::read_to_string;
+use tinhir::{parse, Error};
 
 fn main() -> Result<()> {
     let file_name = args_os().nth(1).ok_or(Error::NoFile)?;
